d65f1a10ff033b891576343b25e0eee576149ab2
Edu <emartinez.virtualcave@external.barcelo.com>
2023-09-16 12:20:11 +0200

    Added module boot

---
+++ naptilus-app-boot/src/main/java/org/naptilus/NapptilusApp.java
@@ -0,0 +1,12 @@
+package org.naptilus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NapptilusApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NapptilusApp.class, args);
+  }
+}
