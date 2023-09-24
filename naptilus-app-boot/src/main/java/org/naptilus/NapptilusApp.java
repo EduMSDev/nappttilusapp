f1f83b243f2ed43f1f769b29c57cd21333b23db7
Edu <emartinez.virtualcave@external.barcelo.com>
2023-09-24 23:20:45 +0200

    Added H2 settings

--- naptilus-app-boot/src/main/java/org/naptilus/NapptilusApp.java
+++ naptilus-app-boot/src/main/java/org/naptilus/NapptilusApp.java
@@ -3,7 +3,7 @@ package org.naptilus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.naptilus")
 public class NapptilusApp {
 
   public static void main(String[] args) {
--- naptilus-app-domain/src/main/java/org/naptilus/domain/Price.java
+++ naptilus-app-domain/src/main/java/org/naptilus/domain/Price.java
@@ -1,5 +1,26 @@
 package org.naptilus.domain;
 
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Price {
 
+  int brandId;
+
+  LocalDate startDate;
+
+  LocalDate endDate;
+
+  int priceList;
+
+  int productId;
+
+  int priority;
+
+  double cost;
+
+  String currency;
 }
